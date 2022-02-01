90bc8add5adcb03e642a056ca036a46c6039d710
jstatus1 <johnsontothetran1@gmail.com>
2022-02-01 10:17:28 -0600

    Module 1 Update

--- api/app.js
+++ api/app.js
@@ -1,17 +1,13 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
 
+const app = express();
 
-const app = express()
 const pathToIndex = path.resolve(__dirname, '../client/index.html');
 
-//C:\Users\Johnson Tran\Documents\Projects\Node Projects\node-express-image-processing-server\api\index.js
-
 app.use('/*', (request, response) => {
-    console.log(__dirname)
-    response.sendFile(pathToIndex)
-})
+  response.sendFile(pathToIndex);
+});
 
-module.exports = app
-
+module.exports = app;
 
